2d9863adc1fbb88b963066297b1b64af4e0a82e7
Vijay6767 <patilvijaysing2092@gmail.com>
2023-07-15 16:48:20 +0530

    Done

--- script.js
+++ script.js
@@ -8,20 +8,45 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  const developers = arr.filter((employee) => employee.profession === "developer");
+  console.log(developers);
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newEmployee  = {
+    id : 4,
+    name :"Vijay",
+    age : "22",
+    profession : "intern"
+  };
+  arr.push(newEmployee);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  const filterdArr = arr.filter((employee) => employee.profession !== "admin");
+  console.log(filterdArr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArray =[
+    {id :5, name: "Ram" , age : "21" , profession: "UI/UX designer"},
+    {id :6, name: "Shaym" , age : "22" , profession: "Manager"},
+    {id :7, name: "Sita" , age : "23" , profession: "Jr.Developer"}
+  ];
+
+  const concatenateArray = arr.concat(newArray);
+  console.log(concatenateArray);
 }
